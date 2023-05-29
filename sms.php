bf87237d9d3f16b158d6984d04368c5cebe6161a
Sweet0Dream <name>
2023-05-29 13:02:26 +0400

    fix route

--- sms.php
+++ sms.php
@@ -1,3 +1,5 @@
 <?php
-    require_once('vendor.php');
+    require_once('func/main.php');
+    require_once('conf/main.php');
+    require_once('vendor/autoload.php');
     echo json_encode(notify_send(), JSON_UNESCAPED_UNICODE);

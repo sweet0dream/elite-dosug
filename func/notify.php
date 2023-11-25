05c4f3c8d7f91ea3db26c482244a57322d60122b
sweet0dream <sweet0dream@proton.me>
2023-11-25 14:56:20 +0400

    notify sms to rest

--- func/notify.php
+++ func/notify.php
@@ -1,13 +1,34 @@
 <?php
-    // notify sms
-    function notify_sms($text, $phone) {
-        if(db_connect()->insert('notify_sms', [
-            'sms_id' => date('YmdHis').rand(111,999),
-            'phone' => $phone,
-            'text' => $text
-        ])) {
-            return true;
-        } else {
-            return false;
-        }
+    /**
+     * @param string $text
+     * @param int $phone
+     * @return bool
+     *
+     * @depricated
+     *  if(db_connect()->insert('notify_sms', [
+     *       'sms_id' => date('YmdHis').rand(111,999),
+     *       'phone' => $phone,
+     *       'text' => $text
+     *  ])) {
+     *       return true;
+     *  } else {
+     *       return false;
+     *  }
+     *
+     */
+    function notify_sms(string $text, int $phone): bool
+    {
+        return file_get_contents(
+            'https://api.elited.ru/notify/sms/add/',
+            true,
+            stream_context_create([
+            'http' => [
+                'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method' => 'POST',
+                'content' => json_encode([
+                    'phone' => $phone,
+                    'text' => $text
+                ]),
+            ],
+        ]));
     }

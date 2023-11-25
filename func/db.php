4a862136759e05f7b4118a8e293a12f0c0b1a13b
sweet0dream <sweet0dream@proton.me>
2023-11-25 14:54:20 +0400

    added remote connect db

--- func/db.php
+++ func/db.php
@@ -3,6 +3,7 @@
         global $db;
         return new PDODb([
             'type' => 'mysql',
+            'host' => '81.31.244.196',
             'username' => $db['user'], 
             'password' => $db['password'],
             'dbname'=> $db['name'],

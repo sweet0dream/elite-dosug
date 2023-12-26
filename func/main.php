0a133ea635266d8ba58f5e9ea8ad558c98a59682
sweet0dream <sweet0dream@proton.me>
2023-12-26 17:13:24 +0400

    refactor func formate_date()

--- func/main.php
+++ func/main.php
@@ -59,7 +59,43 @@
 
 	// date
 	function format_date($date) {
-		return (date('d')==explode('-', explode(' ', $date)[0])[2]?'сегодня':(date('d',time()-86400)==explode('-', explode(' ', $date)[0])[2]?'вчера':explode('-', explode(' ', $date)[0])[2].' '.['01'=>'января','02'=>'февраля','03'=>'марта','04'=>'апреля','05'=>'мая','06'=>'июня','07'=>'июля','08'=>'августа','09'=>'сентября','10'=>'октября','11'=>'ноября','12'=>'декабря'][explode('-', explode(' ', $date)[0])[1]])).(date('Y')!=explode('-', explode(' ', $date)[0])[0]?' '.explode('-', explode(' ', $date)[0])[0].' года':'').' в '.explode(':', explode(' ', $date)[1])[0].':'.explode(':', explode(' ', $date)[1])[1];
+		$termMonth = [
+			'01' => 'января',
+			'02' => 'февраля',
+			'03' => 'марта',
+			'04' => 'апреля',
+			'05' => 'мая',
+			'06' => 'июня',
+			'07' => 'июля',
+			'08' => 'августа',
+			'09' => 'сентября',
+			'10' => 'октября',
+			'11' => 'ноября',
+			'12' => 'декабря'
+		];
+
+		$dateDay = explode(' ', $date)[0];
+		$timeDay = explode(':', explode(' ', $date)[1])[0].':'.explode(':', explode(' ', $date)[1])[1];
+
+		$day = explode('-', $dateDay)[2];
+		$month = explode('-', $dateDay)[1];
+		$year = explode('-', $dateDay)[0];
+
+		if (date('d') == $day && date('m') == $month && date('Y') == $year) {
+			$result = 'сегодня';
+		} elseif (date('d',time()-86400) == $day && date('m') == $month && date('Y') == $year) {
+			$result = 'вчера';
+		} else {
+			$result = ltrim($day, 0).' '.$termMonth[$month];
+		}
+
+		if (date('Y') != $year) {
+			$result .= ' '.$year.' года';
+		}
+
+		$result .= ' в '.$timeDay;
+
+		return $result;
 	}
 
 	// phone

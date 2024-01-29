456a9d2b6fd285012c4c51dba214e0a1d502a1eb
sweet0dream <sweet0dream@proton.me>
2024-01-30 00:52:16 +0400

    change price

--- conf/price.php
+++ conf/price.php
@@ -1,18 +1,18 @@
 <?php
     $price_ank = [
-        'activation' => 100,
-        'blank' => 50,
-        'vip' => 50,
-        'premium' => 50,
-        'top' => 200,
+        'activation' => 50,
+        'blank' => 10,
+        'vip' => 20,
+        'premium' => 20,
+        'top' => 100,
         'delete_review' => 500
     ];
 
     $price_adv = [
         'create_static' => 600,
         'create_animate' => 1200,
-        'b0' => 4500,
-        'b1' => 3500,
-        'b2' => 2500,
-        'b3' => 1500
+        'b0' => 2500,
+        'b1' => 2000,
+        'b2' => 1500,
+        'b3' => 1000
     ];

2e90df82f1d6bc99766c436644a068a3fd6991ef
sweet0dream <sweet0dream@proton.me>
2024-03-04 12:59:27 +0400

    added touch icon

--- design/head.php
+++ design/head.php
@@ -33,6 +33,25 @@
 
 	<link rel="icon" href="/assets/images/favicon.ico" type="image/x-icon">
 	<link rel="shortcut icon" href="/assets/images/favicon.ico" type="image/x-icon">
+
+	<!--touchicon-->
+	<link rel="apple-touch-icon" sizes="57x57" href="/assets/touch-icons/<?= $city['name'] ?>/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="/assets/touch-icons/<?= $city['name'] ?>/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="/assets/touch-icons/<?= $city['name'] ?>/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="/assets/touch-icons/<?= $city['name'] ?>/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="/assets/touch-icons/<?= $city['name'] ?>/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="/assets/touch-icons/<?= $city['name'] ?>/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="/assets/touch-icons/<?= $city['name'] ?>/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="/assets/touch-icons/<?= $city['name'] ?>/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/assets/touch-icons/<?= $city['name'] ?>/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192" href="/assets/touch-icons/<?= $city['name'] ?>/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/assets/touch-icons/<?= $city['name'] ?>/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="/assets/touch-icons/<?= $city['name'] ?>/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/assets/touch-icons/<?= $city['name'] ?>/favicon-16x16.png">
+	<link rel="manifest" href="/assets/touch-icons/<?= $city['name'] ?>/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="/assets/touch-icons/<?= $city['name'] ?>/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">                                                    
 	
 	<script src="/assets/jquery.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="/assets/bootstrap.min.css">
